9995d722be539c926a34ad8963af1f4c4d3473c9
jxbbbb <jxb200136@gmail.com>
2025-03-12 11:42:33 +0800

    reids最大id

--- shorturl_crontab/main.go
+++ shorturl_crontab/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"shorturl_crontab/cron"
+	"shorturl_crontab/pkg/config"
+	"shorturl_crontab/pkg/db/mysql"
+	"shorturl_crontab/pkg/db/redis"
+	"shorturl_crontab/pkg/log"
+)
 
+var (
+	configFile = flag.String("config", "dev.config.yaml", "")
+)
+
+func main() {
+	flag.Parse()
+	//初始化配置文件
+	config.InitConfig(*configFile)
+	cnf := config.GetConfig()
+
+	log.SetLevel(cnf.Log.Level)
+	log.SetOutput(log.GetRotateWriter(cnf.Log.LogPath))
+	log.SetPrintCaller(true)
+
+	//初始化mysql
+	mysql.InitMysql(cnf)
+	//初始化redis
+	redis.InitRedisPool(cnf)
+
+	cron.Run()
 }
